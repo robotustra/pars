bdc4f21cd847e8ea27a89bc49e464a71e4907ab0
Nickolas Yakutovich <yakutovich@yakutovich732.Advantech.ca>
2013-10-10 08:52:58 -0400

    Start point of parser.

---
+++ README.md
@@ -0,0 +1,2 @@
+This is a parser to investigate the structure of the asm project and to build
+the tree of calls.
---
+++ parser.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+/* Getting the first file as argument to start scanning with the option -file=<filename> .
+   or with the first procedure -proc=<procname> */
+
+
+
+
+int main(int argc, char* argv[])
+{
+	printf("Hello, world!\n");
+	return 0;
+}
